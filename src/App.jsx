633b6cf8f2990ae9d116e1f6b546bd8ab806f8aa
Marialafu <lafuentelhmaria@gmail.com>
2025-05-22 00:06:12 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
-import Button from './components/button/Button';
-import { GlobalStyles } from './styles/GlobalStyles';
+import { useForm } from "react-hook-form"
+import { GlobalStyles } from "./styles/GlobalStyles";
 
-const App = () => {
+export default function App() {
+
+  const {register, handleSubmit, formState: {errors}} = useForm()
+  //const onSumbit = (data) => console.log(data);
+  console.log(errors);
+  
+
   return (
-    <>
+    
+    <div>
       <GlobalStyles />
-      <Button />
-    </>
-  );
-};
+      <h1>Contact us</h1>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <div>
 
-export default App;
+          <div>
+            <label htmlFor="firstName">First Name *</label>
+            <input 
+            type="text" 
+            id="firstName" 
+            {...register("firstName", {required: 'This field is required'})}/>
+            <p>{errors.firstName?.message}</p>
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name *</label>
+            <input type="text" id="lastName" {...register("LastName", {required: 'This field is required'})}/>
+            <p>{errors.lastName?.message}</p>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="email">Email Adress *</label>
+          <input type="text" id="email"/>
+        </div>
+        <div>
+          <label>Query Type *</label>
+          <div>
+            <input type="checkbox" id="generalEnquiry"/>
+            <label htmlFor="generalEnquiry">General Enquiry</label>
+          </div>
+          <div>
+            <input type="checkbox" id="supportRequest"/>
+            <label htmlFor="supportRequest">Support Request</label>
+          </div>
+        </div>
+
+          <div>
+            <label htmlFor="message">Message *</label>
+            <input type="input" id="message"/>
+          </div>
+          <div>
+            <input type="checkbox" id="contacted"/>
+            <label htmlFor="contacted">I consent to being contacted by the team *</label>
+          </div>
+        <input type="submit" />
+        
+      </form>
+    </div>
+  )
+}
